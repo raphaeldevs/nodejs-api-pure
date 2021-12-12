bd9a3ff317855f84aa7f185add04810260dff3bd
Raphael Corrêa <raphaeldeoliveiracorrea@gmail.com>
2021-12-12 02:36:12 -0300

    feat: create first routes

--- src/index.js
+++ src/index.js
@@ -1,10 +1,30 @@
 const http = require('http')
 
+const users = require('./mocks/users')
+
 const server = http.createServer((request, response) => {
-  response.writeHead(200, {
+  console.log(`Receiving method request ${request.method} at ${request.url}`)
+
+  if (request.url === '/' && request.method === 'GET') {
+    response.writeHead(200, {
+      'Content-Type': 'text/html'
+    })
+
+    return response.end('<h1>Hello NodeJS!</h1>')
+  }
+
+  if (request.url === '/users' && request.method === 'GET') {
+    response.writeHead(200, {
+      'Content-Type': 'application/json'
+    })
+
+    return response.end(JSON.stringify(users))
+  }
+
+  response.writeHead(404, {
     'Content-Type': 'text/html'
   })
-  response.end('<h1>Hello NodeJS!</h1>')
+  return response.end(`Cannot ${request.method} ${request.url}`)
 })
 
 server.listen(3000, () =>
---
+++ src/mocks/users.js
@@ -0,0 +1,42 @@
+module.exports = [
+  {
+    id: 1,
+    name: 'Rapha'
+  },
+  {
+    id: 2,
+    name: 'Pepito'
+  },
+  {
+    id: 3,
+    name: 'Juan'
+  },
+  {
+    id: 4,
+    name: 'Carlos'
+  },
+  {
+    id: 5,
+    name: 'Sara'
+  },
+  {
+    id: 6,
+    name: 'Miguel'
+  },
+  {
+    id: 7,
+    name: 'Pablo'
+  },
+  {
+    id: 8,
+    name: 'Luisa'
+  },
+  {
+    id: 9,
+    name: 'Alberto'
+  },
+  {
+    id: 10,
+    name: 'Maria'
+  }
+]
