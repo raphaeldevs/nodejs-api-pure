247c5fdfeb69cdef14eba278768e4ae7c79bef32
Raphael Corrêa <raphaeldeoliveiracorrea@gmail.com>
2021-12-12 02:18:50 -0300

    feat: create server

---
+++ src/index.js
@@ -0,0 +1,12 @@
+const http = require('http')
+
+const server = http.createServer((request, response) => {
+  response.writeHead(200, {
+    'Content-Type': 'text/html'
+  })
+  response.end('<h1>Hello NodeJS!</h1>')
+})
+
+server.listen(3000, () =>
+  console.log('🎈 Server started at http://localhost:3000')
+)
